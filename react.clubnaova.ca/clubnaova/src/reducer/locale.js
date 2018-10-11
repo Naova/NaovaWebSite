0d90d655c454d527448f735a44d52d60c61cae7d
wmarc04 <wmarc-antoine@hotmail.com>
2018-10-11 12:59:44 -0400

    Check la langue setter dans le navigateur du user et affiche la bonne langue

--- react.clubnaova.ca/clubnaova/src/reducer/locale.js
+++ react.clubnaova.ca/clubnaova/src/reducer/locale.js
@@ -1,6 +1,11 @@
 import { LOCALE_SET } from "../types/locale";
 
-export default function locale(state={lang:"en"}, action={}){
+let lg = "en";
+if(navigator.language.startsWith("fr") || navigator.userLanguage.startsWith("fr")){
+    lg = "fr"
+}
+
+export default function locale(state={lang:lg}, action={}){
     switch(action.type){
         case LOCALE_SET:
             return {lang: action.lang};

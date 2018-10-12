3f36eafc7164218a6643466ed56a76523d3583f0
wmarc04 <wmarc-antoine@hotmail.com>
2018-10-12 16:43:11 -0400

    fix navbar collaspe (mobile et tablette)

--- react.clubnaova.ca/clubnaova/src/components/NavBar/NavBar.jsx
+++ react.clubnaova.ca/clubnaova/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import { FormattedMessage } from "react-intl";
 import { setLocale } from "../../actions/locale";
 import { getLang } from "../../selector/locale";
@@ -10,71 +11,89 @@ import "./navbar.css";
 const logo_img = require("../../img/logo/logo.png");
 
 class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isCollapseNotActive: false };
+    }
 
-    render(){
+    onChangeCollapse = () => {
+        let oldIsCollapseNotActive = this.state.isCollapseNotActive;
+        this.setState({
+            isCollapseNotActive: !oldIsCollapseNotActive
+        })
+    }
+
+    onChangeLangue = () => {
         let langChange = "fr";
         if(this.props.lang === "fr"){
             langChange = "en";
         }
 
+        this.props.setLocale(langChange)
+        this.onChangeCollapse();
+    }
+
+    render(){
+        let navClassName = classNames("navbar-collapse", {"collapse": !this.state.isCollapseNotActive}, {"not_collapse": this.state.isCollapseNotActive})
+
         return(
                 <nav id="nav_bar" className="navbar navbar-default navbar-fixed-top">
                 <div className="container">
                     <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <a className="navbar-brand" href="/"><img src={logo_img} alt="Logo du Club Naova"/></a>
+                    <button onClick={this.onChangeCollapse} type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                         <span className="sr-only">Toggle navigation</span>
                         <span className="icon-bar"></span>
                         <span className="icon-bar"></span>
                         <span className="icon-bar"></span>
                     </button>
-                    <a className="navbar-brand" href="/"><img src={logo_img} alt="Logo du Club Naova"/></a>
                     </div>
         
-                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <div className={navClassName} id="navbar">
                     <ul className="nav navbar-nav navbar-right">
                         <li>
-                            <a href="/#tf-home" className="scroll">
+                            <a href="/#tf-home" onClick={this.onChangeCollapse} className="scroll">
                                 <FormattedMessage id="nav.home" defaultMessage="Home" />
                             </a>
                         </li>
                         <li>
-                            <a href="/#tf-presentation" className="scroll">
+                            <a href="/#tf-presentation" onClick={this.onChangeCollapse} className="scroll">
                                 <FormattedMessage id="nav.presentation" defaultMessage="Presentation" />
                             </a>
                         </li>
                         <li>
-                            <a href="/#tf-history" className="scroll">
+                            <a href="/#tf-history" onClick={this.onChangeCollapse} className="scroll">
                                 <FormattedMessage id="nav.history" defaultMessage="History" />
                             </a>
                         </li>
                         <li>
-                            <a href="/#tf-about" className="scroll">
+                            <a href="/#tf-about" onClick={this.onChangeCollapse} className="scroll">
                                 <FormattedMessage id="nav.projects" defaultMessage="Projects" />
                             </a>
                         </li>
                         <li>
-                            <a href="/#tf-works" className="scroll">
+                            <a href="/#tf-works" onClick={this.onChangeCollapse} className="scroll">
                                 <FormattedMessage id="nav.pictures" defaultMessage="Pictures" />
                             </a>
                         </li>
                         <li>
-                            <a href="/team" className="scroll">
+                            <a href="/team" onClick={this.onChangeCollapse} className="scroll">
                                 <FormattedMessage id="nav.team" defaultMessage="Team" />
                             </a>
                         </li>
                         <li>
-                            <a href="partenaires.html" className="scroll">
+                            <a href="partenaires.html" onClick={this.onChangeCollapse} className="scroll">
                                 <FormattedMessage id="nav.sponsors" defaultMessage="Sponsors" />
                             </a>
                         </li>
                         <li>
-                            <a href="/#tf-contact" className="scroll">
+                            <a href="/#tf-contact" onClick={this.onChangeCollapse} className="scroll">
                                 <FormattedMessage id="nav.contact" defaultMessage="Contact us" />
                             </a>
                         </li> 
                         <li><span className="scroll navbar_small_bar">|</span></li>
                         <li>
-                            <a className="scroll" onClick={() => this.props.setLocale(langChange)}>
+                            <a className="scroll" onClick={this.onChangeLangue}>
                                 <FormattedMessage id="nav.langue" defaultMessage="fr" />
                             </a>
                         </li>

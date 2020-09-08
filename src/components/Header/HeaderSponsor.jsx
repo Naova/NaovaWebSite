8bf80e788dfe0fc9fa7ea381d8ede30cbc794b98
Luss <etienne.lussier.1@etsmtl.net>
2020-09-08 10:01:32 -0400

    Ajouter donation button

--- src/components/Header/HeaderSponsor.jsx
+++ src/components/Header/HeaderSponsor.jsx
@@ -7,6 +7,8 @@ const sponsor_pdf = require("../../doc/partenariat/partenariat_2019.pdf");
 
 const naovoLogo_img = require("../../img/logo/logo_titre_degraderblanc2.png");
 
+const don_link = "https://www.jedonneenligne.org/fdets/campagne/ets/defi/Challenge/view/e79ea1c7-2ad2-494c-b56e-322a89d432a9?fbclid=IwAR04XC9yKNndLICDmI3dIdh2lEvDJQ2E2BLZMkaBrftD0lIiQKm98W3js_8";
+
 class headerSponsor extends Component {
 
     render(){
@@ -24,6 +26,11 @@ class headerSponsor extends Component {
                                 <FormattedMessage id="sponsors.becomeSponsors" defaultMessage="Become a sponsors!" />
                             </a>
                         </center>
+                        <center>
+                            <a href={don_link} target="_blank" type="button" className="btn btn-primary btn_become_sponsor">
+                                <FormattedMessage id="sponsors.donation" defaultMessage="Make a donation!" />
+                            </a>
+                        </center>
                     </div> 
                 </div>
             </div>
@@ -31,4 +38,4 @@ class headerSponsor extends Component {
     }
 }
 
-export default HeaderPartenaire;
+export default headerSponsor;

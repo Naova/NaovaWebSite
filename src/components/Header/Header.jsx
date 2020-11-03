7ccceb997ba4a7b1c8ba0c7f97f00b868aabcd62
Girardeau Vincent <vgirardeau@users.noreply.github.com>
2020-11-03 10:09:24 -0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,6 @@ import "./header.css"
 
 const naovoLogo_img = require("../../img/logo/logo_titre_degraderblanc2.png");
 const aeetsLogo_img = require("../../img/partenaire/aeetslogo.png");
-const ubiMtlLogo_img = require("../../img/partenaire/Ubisoft_Montreal_Logo.png");
 const etsLogo_img = require("../../img/partenaire/ets.png");
 const equisoft_img = require("../../img/partenaire/EQUISOFT_RGB_FR.png");
 

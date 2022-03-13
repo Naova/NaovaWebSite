83094efb229591ff84334a8e8a3cf98452bbefa8
Mayo <mayorathan@outlook.com>
2022-03-13 11:08:03 -0400

    Added sherweb (#45)

--- src/components/Sponsors/Argent.jsx
+++ src/components/Sponsors/Argent.jsx
@@ -4,26 +4,41 @@ import SectionHeaderNao from "../SectionHeaderNao/SectionHeaderNao";
 import Sponsors from "./Sponsors";
 
 class Argent extends Component {
-    render() {
-        return (
-            <div>
-                <SectionHeaderNao isBigNaoFace={false}>
-                    <FormattedMessage id="sponsors.silver" defaultMessage="Silver" />
-                </SectionHeaderNao>
+  render() {
+    return (
+      <div>
+        <SectionHeaderNao isBigNaoFace={false}>
+          <FormattedMessage id='sponsors.silver' defaultMessage='Silver' />
+        </SectionHeaderNao>
 
-                <div className="container">
-                    <div className="row align-items-center">
-                        <Sponsors 
-                            img="fdrdqlogo.png"
-                            link="http://www.frqnt.gouv.qc.ca/accueil"
-                            colNum="12"
-                            idTitle="sponsors.sp3"
-                            defaultMessageTitle="Fond de recherche du Québec"/>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+        <div className='container'>
+          <div className='row align-items-center'>
+            {/* <Sponsors
+                  img='fdrdqlogo.png'
+                  link='http://www.frqnt.gouv.qc.ca/accueil'
+                  colNum='12'
+                  idTitle='sponsors.sp3'
+                  defaultMessageTitle='Fond de recherche du Québec'
+                />{" "} */}
+            <Sponsors
+              img='sherweb_logo.png'
+              link='https://www.sherweb.com/'
+              colNum='12'
+              idTitle='sponsors.sp7'
+              defaultMessageTitle='Sherweb'
+            />{" "}
+            <Sponsors
+              img='octasic_logo.png'
+              link='http://www.octasic.com/'
+              colNum='12'
+              idTitle='sponsors.sp6'
+              defaultMessageTitle='Octasic'
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Argent;
--- src/components/Sponsors/Or.jsx
+++ src/components/Sponsors/Or.jsx
@@ -4,33 +4,26 @@ import SectionHeaderNao from "../SectionHeaderNao/SectionHeaderNao";
 import Sponsors from "./Sponsors";
 
 class Or extends Component {
-    render() {
-        return (
-          <div>
-            <SectionHeaderNao isBigNaoFace={false}>
-              <FormattedMessage id='sponsors.gold' defaultMessage='Gold' />
-            </SectionHeaderNao>
-            <div className='container'>
-              <div className='justify-content-center row'>
-                {/* <Sponsors
+  render() {
+    return (
+      <div>
+        <SectionHeaderNao isBigNaoFace={false}>
+          <FormattedMessage id='sponsors.gold' defaultMessage='Gold' />
+        </SectionHeaderNao>
+        <div className='container'>
+          <div className='justify-content-center row'>
+            {/* <Sponsors
                   img='aeetslogo.png'
                   link='http://aeets.com/'
                   colNum='6'
                   idTitle='sponsors.sp1'
                   defaultMessageTitle="ETS's Student Association"
                 /> */}
-                <Sponsors
-                  img='octasic_logo.png'
-                  link='http://www.octasic.com/'
-                  colNum='6'
-                  idTitle='sponsors.sp6'
-                  defaultMessageTitle="Octasic"
-                />
-              </div>
-            </div>
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Or;

63f581d7443a272e53001931982f6076130dcc95
Tony <Tony@Tonys-MacBook-Air.local>
2023-10-14 13:27:14 -0400

    Displays logo_titre.svg on main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,56 +1,92 @@
 import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 
-import "./header.css"
+import "./header.css";
 
-const naovaLogo_img = require("../../img/logo/logo_titre.svg");
+const naovaLogo_img = require("../../img/logo/logo_titre.svg").default;
 const aeetsLogo_img = require("../../img/partenaire/aeetslogo.png");
 const etsLogo_img = require("../../img/partenaire/ets.png");
 const teledyneLogo_img = require("../../img/partenaire/Teledyne_Logo_Blue.png");
 const rousseauLogo_img = require("../../img/partenaire/rousseau_blue_logo.png");
 
 class Header extends Component {
-
-    render(){
-        return(
-            <div id="tf-home">
-                <div className="overlay"> 
-                    <div className="container">
-                        <div className="content-heading text-center">
-                            <center><img src={naovaLogo_img} className="intro-logo img-responsive" alt="Naova Logo"/></center>
-                            <h1><FormattedMessage id="header.title" defaultMessage="Robotic scientific club" /></h1>
-                            <p className="lead"><FormattedMessage id="header.school" defaultMessage="Ecole de Technologie Supérieure, Montréal" /></p>
-                            <div className="col-md-12 row align-items-center partenaire-wrapper">
-                                <div className="col-sm">
-                                    <a href="https://www.etsmtl.ca/" >
-                                        <img src={etsLogo_img} className="partenaire_ets_img" alt="École de technologie supérieure"/>
-                                    </a>
-                                </div>
-                                <div className="col-sm">
-                                    <a href="https://www.aeets.com/" >
-                                        <img src={aeetsLogo_img} className="partenaire_aeets_img" alt="AEETS"/>
-                                    </a>
-                                </div> 
-                                <div className="col-sm">
-                                    <a href="https://www.rousseau.com/int_fr/" >
-                                        <img src={rousseauLogo_img} className="partenaire_rousseau_img" alt="Rousseau"/>
-                                    </a>
-                                </div>
-                                <div className="col-sm">
-                                    <a href="https://www.teledyne.com/" >
-                                        <img src={teledyneLogo_img} className="partenaire_teledyne_img" alt="Teledyne"/>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="col-md-12">
-                                <a href="#tf-presentation" className="scroll goto-arrow"><i className=" fa fa-arrow-down fa-3x"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div id="tf-home">
+				<div className="overlay">
+					<div className="container">
+						<div className="content-heading text-center">
+							<center>
+								<img
+									src={naovaLogo_img}
+									className="intro-logo img-responsive"
+									alt="Naova Logo"
+								/>
+							</center>
+							<h1>
+								<FormattedMessage
+									id="header.title"
+									defaultMessage="Robotic scientific club"
+								/>
+							</h1>
+							<p className="lead">
+								<FormattedMessage
+									id="header.school"
+									defaultMessage="Ecole de Technologie Supérieure, Montréal"
+								/>
+							</p>
+							<div className="col-md-12 row align-items-center partenaire-wrapper">
+								<div className="col-sm">
+									<a href="https://www.etsmtl.ca/">
+										<img
+											src={etsLogo_img}
+											className="partenaire_ets_img"
+											alt="École de technologie supérieure"
+										/>
+									</a>
+								</div>
+								<div className="col-sm">
+									<a href="https://www.aeets.com/">
+										<img
+											src={aeetsLogo_img}
+											className="partenaire_aeets_img"
+											alt="AEETS"
+										/>
+									</a>
+								</div>
+								<div className="col-sm">
+									<a href="https://www.rousseau.com/int_fr/">
+										<img
+											src={rousseauLogo_img}
+											className="partenaire_rousseau_img"
+											alt="Rousseau"
+										/>
+									</a>
+								</div>
+								<div className="col-sm">
+									<a href="https://www.teledyne.com/">
+										<img
+											src={teledyneLogo_img}
+											className="partenaire_teledyne_img"
+											alt="Teledyne"
+										/>
+									</a>
+								</div>
+							</div>
+							<div className="col-md-12">
+								<a
+									href="#tf-presentation"
+									className="scroll goto-arrow"
+								>
+									<i className=" fa fa-arrow-down fa-3x"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Header;
--- src/components/Team/Member.jsx
+++ src/components/Team/Member.jsx
@@ -6,105 +6,170 @@ import { getLang } from "../../selector/locale";
 
 import "./member.css";
 
-const naoFace_img = require("../../img/logo/naoface.svg");
+const naoFace_img = require("../../img/logo/naoface.svg").default;
 
 class Member extends Component {
+	renderImageMember() {
+		const { imgName, name } = this.props;
+		if (imgName) {
+			return (
+				<img
+					src={require(`../../img/team/${imgName}.jpg`)}
+					alt={name}
+					className="img-fluid img-membre-profil"
+				/>
+			);
+		} else {
+			return (
+				<img
+					src={naoFace_img}
+					alt="team member"
+					className="img-fluid"
+				/>
+			);
+		}
+	}
 
-    renderImageMember() {
-        const { imgName, name } = this.props;
-        if (imgName) {
-            return <img src={require(`../../img/team/${imgName}.jpg`)} alt={name} className="img-fluid img-membre-profil" />;
-        } else {
-            return <img src={naoFace_img} alt="team member" className="img-fluid" />;
-        }
-    }
+	renderFounder() {
+		const { isFounder } = this.props;
+		if (isFounder) {
+			return (
+				<span>
+					<br />
+					<span>
+						<FormattedMessage
+							id="team.founder"
+							defaultMessage="Founder"
+						/>
+					</span>
+				</span>
+			);
+		}
+	}
 
-    renderFounder() {
-        const { isFounder } = this.props;
-        if (isFounder) {
-            return <span><br /><span><FormattedMessage id="team.founder" defaultMessage="Founder" /></span></span>;
-        }
-    }
+	renderProgram() {
+		const { lang, idProgramme, defaultMessageProgramme } = this.props;
+		if (lang === "fr") {
+			return (
+				<span>
+					<span className="team-student">
+						<FormattedMessage
+							id="team.student"
+							defaultMessage="Étudiant en "
+						/>
+					</span>
+					<span>
+						<FormattedMessage
+							id={idProgramme}
+							defaultMessage={defaultMessageProgramme}
+						/>
+					</span>
+				</span>
+			);
+		} else {
+			return (
+				<span>
+					<span>
+						<FormattedMessage
+							id={idProgramme}
+							defaultMessage={defaultMessageProgramme}
+						/>
+					</span>
+					<span className="team-student">
+						<FormattedMessage
+							id="team.student"
+							defaultMessage="student"
+						/>
+					</span>
+				</span>
+			);
+		}
+	}
 
-    renderProgram() {
-        const { lang, idProgramme, defaultMessageProgramme } = this.props;
-        if (lang === "fr") {
-            return (
-                <span>
-                    <span className="team-student"><FormattedMessage id="team.student" defaultMessage="Étudiant en " /></span>
-                    <span><FormattedMessage id={idProgramme} defaultMessage={defaultMessageProgramme} /></span>
-                </span>);
-        } else {
-            return (
-                <span>
-                    <span><FormattedMessage id={idProgramme} defaultMessage={defaultMessageProgramme} /></span>
-                    <span className="team-student"><FormattedMessage id="team.student" defaultMessage="student" /></span>
-                </span>);
-        }
-    }
+	renderTitre() {
+		const { idTitre, defaultMessageTitre } = this.props;
+		if (idTitre && defaultMessageTitre) {
+			const titres = idTitre.split("&").map((titre) => titre.trim());
 
-    renderTitre() {
-        const { idTitre, defaultMessageTitre } = this.props;
-        if (idTitre && defaultMessageTitre) {
+			return (
+				<span>
+					<span>
+						{titres.map((titre, index) => {
+							console.log(titre);
 
-            const titres = idTitre.split("&").map((titre) => titre.trim());
+							// Ajout si ce n'est pas le dernier titre
+							const ajoutAmpersand =
+								index !== titres.length - 1 ? (
+									<span> & </span>
+								) : (
+									<React.Fragment />
+								);
 
-            return (<span><span>{
-                titres.map((titre, index) => {
-                    console.log(titre)
+							return (
+								<>
+									<FormattedMessage
+										id={titre}
+										defaultMessage={defaultMessageTitre}
+									/>
+									{ajoutAmpersand}
+								</>
+							);
+						})}
+					</span>
+					<br />
+				</span>
+			);
+		}
+	}
 
-                    // Ajout si ce n'est pas le dernier titre
-                    const ajoutAmpersand = index !== titres.length -1 ? <span> & </span> : <React.Fragment />;
-
-                    return <>
-                        <FormattedMessage id={titre} defaultMessage={defaultMessageTitre} />
-                        {ajoutAmpersand}
-                    </>
-                })
-            }</span><br /></span>);
-        }
-    }
-
-    render() {
-
-        const { name, isCurrentMember, linkedin } = this.props;
-        return (
-            <div className="col-md-4 col-sm-4 card-team-member">
-                <div className="team-member">
-                    <div className="team-img team-member-img">
-                        {this.renderImageMember()}
-                    </div>
-                </div>
-                <div className="team-title">
-                    <h5>{name}</h5>
-                    <p className="m-0">
-                        {isCurrentMember && <a href={linkedin} target="_blank" rel="noopener noreferrer" class="mx-1"><i className="fa fa-linkedin"></i></a>}
-                    </p>
-                    {this.renderTitre()}
-                    {this.renderProgram()}
-                    {this.renderFounder()}
-                </div>
-            </div>
-        );
-    }
+	render() {
+		const { name, isCurrentMember, linkedin } = this.props;
+		return (
+			<div className="col-md-4 col-sm-4 card-team-member">
+				<div className="team-member">
+					<div className="team-img team-member-img">
+						{this.renderImageMember()}
+					</div>
+				</div>
+				<div className="team-title">
+					<h5>{name}</h5>
+					<p className="m-0">
+						{isCurrentMember && (
+							<a
+								href={linkedin}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="mx-1"
+							>
+								<i className="fa fa-linkedin"></i>
+							</a>
+						)}
+					</p>
+					{this.renderTitre()}
+					{this.renderProgram()}
+					{this.renderFounder()}
+				</div>
+			</div>
+		);
+	}
 }
 
 Member.propTypes = {
-    imgName: PropTypes.string,
-    img2Name: PropTypes.string,
-    name: PropTypes.string,
-    idProgramme: PropTypes.string,
-    defaultMessageProgramme: PropTypes.string,
-    idTitre: PropTypes.string,
-    defaultMessageTitre: PropTypes.string,
-    isFounder: PropTypes.bool,
-    lang: PropTypes.string.isRequired,
-}
+	imgName: PropTypes.string,
+	img2Name: PropTypes.string,
+	name: PropTypes.string,
+	idProgramme: PropTypes.string,
+	defaultMessageProgramme: PropTypes.string,
+	idTitre: PropTypes.string,
+	defaultMessageTitre: PropTypes.string,
+	isFounder: PropTypes.bool,
+	lang: PropTypes.string.isRequired,
+};
 
 const mapStateToProps = (state) => {
-    return {
-        lang: getLang(state)
-    };
-}
+	return {
+		lang: getLang(state),
+	};
+};
 
 export default connect(mapStateToProps, null)(Member);

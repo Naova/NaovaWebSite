74c6357687c1d9aeed89cf5c4936ea69f11c0f28
Emmanuel Coulombe <emmanuelcoulombe@civalgo.com>
2024-03-23 14:06:40 -0400

    fini toutes les photos

--- src/components/Press/PressCard.jsx
+++ src/components/Press/PressCard.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 
+const getImageLink = (article) => {
+  const isYoutube = article.link.toLowerCase().includes('youtube')
+
+  if(isYoutube) {
+    
+    const index = article.link.lastIndexOf("=");
+    const videoId = article.link.substring(index+1, article.link.length)
+    
+    return `https://i3.ytimg.com/vi/${videoId}/maxresdefault.jpg`
+  }
+
+  if(article.image) {
+    return require(`../../img/press/${article.image.toLowerCase()}`)
+  }
+
+  return require("../../img/logo/naoface.svg").default;
+
+}
+
+
 const PressCard = ({article}) => {
   const date = new Date(article.date)
-  const naoFace_img = require("../../img/logo/naoface.svg").default;
+  
+  const image = getImageLink(article)
 
   return (
-    <div className="press-card">
+    <div className="press-card" onClick={() => window.open(article.link, "_blank")}>
         <p className="press-card-title">{article.title}</p>
         <p className="press-card-date">{date.toLocaleDateString()}</p>
         <p className="press-card-summary">{article.resume}</p>
-        <a href={article.link} target="_blank" rel="noopener noreferrer" className="press-card-link">Read the article</a>
         <p className="press-card-source">Source: {article.source}</p>
-        <img src={naoFace_img} className="press-card-img" />
+        <img src={image} className="press-card-img" />
     </div>
+    
   )
 }
 
